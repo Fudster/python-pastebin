19ccc8b7003e75a5c9ff86a40ea8753137367075
Fudster <yofun111@gmail.com>
2016-12-03 01:40:08 +0000

    Implent a "better" way of aceesing the DB...

--- pastebin.py
+++ pastebin.py
@@ -10,15 +10,25 @@ app = Flask(__name__, static_url_path='')
 def app_start(host,port):
     app.run(host=host, port=port)
 
-def query(sql, params=None, commitx=False):
-    conn = sqlite3.connect('paste.db')
-    cursor = conn.cursor()
-    cursor.execute(sql, params)
-    data = cursor.fetchall()
-    if commitx is True:
-        conn.commit()
-    cursor.close()
-    return data
+DATABASE = 'paste.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+        
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 def make_id():
 	return  "".join(sample((ascii_letters) * 10, 10))
